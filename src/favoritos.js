00b54c64509a94289db1e4b6b231c028fd8ea203
genaromeynet <genaromeynet@gmail.com>
2026-06-24 15:32:18 -0300

    agrega funcionalidad de quitar equipo desde favoritos

--- src/favoritos.js
+++ src/favoritos.js
@@ -1,16 +1,33 @@
 const contenedor = document.getElementById("favoritos");
 
-const favoritos =
+let favoritos =
     JSON.parse(localStorage.getItem("favoritos")) || [];
-
 
 favoritos.forEach(equipo => {
 
-contenedor.innerHTML += `
-        <div class="card-equipo">
-            <img src="${equipo.logo}">
-            <h3>${equipo.name}</h3>
-        </div>
-    `;
-});
+    const card = document.createElement("div");
+    card.classList.add("card-equipo");
 
+    card.innerHTML = `
+        <img src="${equipo.logo}">
+        <h3>${equipo.name}</h3>
+    `;
+
+    const btn = document.createElement("button");
+    btn.textContent = "❌";
+
+    btn.addEventListener("click", () => {
+
+        let lista =
+            JSON.parse(localStorage.getItem("favoritos")) || [];
+
+        lista = lista.filter(f => f.id !== equipo.id);
+
+        localStorage.setItem("favoritos", JSON.stringify(lista));
+
+        card.remove(); // ✔ ahora siempre funciona
+    });
+
+    card.appendChild(btn);
+    contenedor.appendChild(card);
+});
